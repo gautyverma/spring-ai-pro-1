f3682d669a97e7c4935438f1178563bae90c1762
GAUTY <gautyverma@gmail.com>
2025-07-27 22:28:01 +0530

    [Spring-AI] - base setup and API-key configured

---
+++ src/main/java/com/matuga/ai/SpringAiPro1Application.java
@@ -0,0 +1,13 @@
+package com.matuga.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPro1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matuga/ai/SpringAiPro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matuga.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
