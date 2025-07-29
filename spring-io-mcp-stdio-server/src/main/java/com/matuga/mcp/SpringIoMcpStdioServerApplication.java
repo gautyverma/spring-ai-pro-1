58304dde45788d0af4e53748a14db02fa016324f
GAUTY <gautyverma@gmail.com>
2025-07-29 23:10:53 +0530

    [spring-mcp]-initial commit and application config setup

---
+++ spring-io-mcp-stdio-server/src/main/java/com/matuga/mcp/SpringIoMcpStdioServerApplication.java
@@ -0,0 +1,13 @@
+package com.matuga.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoMcpStdioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoMcpStdioServerApplication.class, args);
+	}
+
+}
---
+++ spring-io-mcp-stdio-server/src/test/java/com/matuga/mcp/SpringIoMcpStdioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matuga.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoMcpStdioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
